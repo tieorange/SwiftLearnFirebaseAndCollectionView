48a1bb9e35482260203949757667925c63c4b5b0
Andrii Kovalchuk <tieorange@gmail.com>
2017-04-24 22:54:17 +0200

    Initial Commit

---
+++ LearnFirebaseTry2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearnFirebaseTry2
+//
+//  Created by Andrii Kovalchuk on 24/04/2017.
+//  Copyright © 2017 Andrii Kovalchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
